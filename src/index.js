b609b360100b2a4017c5577cb90d8d3fc2e3538f
Blanpain <georges@visite-editions.com>
2016-07-07 11:00:58 +0200

    modifcation options

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 //-----------------------------
 // Require
 //-----------------------------
+var fs = require("fs");
 var cliargs = require('cliargs');
 var tools = require('./tools');
-var argsObj = cliargs.parse();
-var passerelles = [
-	"soyez"
-];
+var passerelles = require('./liste_passerelles.json');
 
 //-----------------------------
 // Define
@@ -15,23 +13,47 @@ var path_assets = "C:/tools/visiteimmo/passerelles/assets/";
 var path_tempo = path_assets + "tempo/";
 var path_input = path_assets + "input/";
 var path_output = path_assets + "output/";
+var argsObj = cliargs.parse();
 
 //-----------------------------
 // Console
 //-----------------------------
 if(argsObj.a)
 {
-	for(var i in passerelles)
-	{
-		traitement_passerelle(passerelles[i]);			
-	}
+	convertir_toutes_les_passerelles();
 }
 
 if(argsObj.p)
 {
-	if(passerelles.indexOf(argsObj.p) != -1)
+	convertir_une_passerelles(argsObj.p);
+}
+
+if(argsObj.h)
+{
+	help();
+}
+
+//-----------------------------
+// Convertir
+//-----------------------------
+function convertir_toutes_les_passerelles()
+{
+	for(var i in passerelles)
 	{
-		traitement_passerelle(argsObj.p);	
+		convertir_une_passerelles(passerelles[i]);			
+	}	
+}
+
+function convertir_une_passerelles(cible)
+{
+	if(passerelles.indexOf(cible) != -1)
+	{
+		var passerelle = require('./format/'+cible+'.js');
+		var path_passerelle = path_input+cible+"/";
+		var path_passerelle_json = path_output+"/"+cible+".json";
+		var annonces = tools.validations(passerelle.convertir(path_passerelle));
+		fs.writeFileSync(path_passerelle_json, JSON.stringify(annonces), "UTF-8");
+		console.log(tools.bilan(annonces));		
 	}
 	else
 	{
@@ -40,25 +62,21 @@ if(argsObj.p)
 }
 
 
-function traitement_passerelle(cible)
-{
-	var passerelle = require('./format/'+cible+'.js');
-	var path_passerelle = path_input+cible+"/";
-	var annonces = tools.validations(passerelle.convertir(path_passerelle));
-	console.log(tools.bilan(annonces));
-	//fs.writeFileSync(path_assets+"cible.sql", all_sql, "UTF-8");	
-}
-
 //-----------------------------
-// Help
+// Aide
 //-----------------------------
-if(argsObj.h)
+function help()
 {
 	console.log("===== GESTIONAIRE passerelle ==========");
 	console.log("-a : importer tout les passerelles");
 	console.log("--p=<nom_passrelle> : importer une passerelle specifique");
 	console.log("=======================================");
 }
+
+
+exports.convertir_toutes_les_passerelles = convertir_toutes_les_passerelles;
+exports.convertir_une_passerelles = convertir_une_passerelles;
+exports.help = help;
 
 
 
